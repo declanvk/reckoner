5bc43c6e64f2944c46da95a3bd01e86b0435e7fa
Declan Kelly <dkelly.home@gmail.com>
2019-11-03 20:29:24 -0800

    Add divide operations
    Reorganize divide and remainder shared code.

--- src/integer/ops.rs
+++ src/integer/ops.rs
@@ -1,5 +1,5 @@
 use crate::integer::Integer;
-use core::ops::{Add, Mul, Neg, Rem, Sub};
+use core::ops::{Add, Div, Mul, Neg, Rem, Sub};
 
 macro_rules! impl_single_binop {
     ($op_path:ident, $op_fn:ident, $celf:ty, $rhs:ty, $fn:path, $ret:ty, ref self, ref rhs) => {
@@ -290,6 +290,80 @@ impl_single_binop!(Mul, mul, &Integer, u128, Integer::multiply, Integer, into rh
 impl_single_binop!(Mul, mul, Integer, &u128, Integer::multiply, Integer, ref self, into rhs);
 impl_single_binop!(Mul, mul, &Integer, &u128, Integer::multiply, Integer, into rhs);
 
+// DIVISION
+
+impl_single_binop!(
+    Div,
+    div,
+    &Integer,
+    Integer,
+    Integer::divide,
+    Integer,
+    ref rhs
+);
+impl_single_binop!(Div, div, Integer, &Integer, Integer::divide, Integer, ref self);
+impl_single_binop!(Div, div, &Integer, &Integer, Integer::divide, Integer);
+impl_single_binop!(Div, div, Integer, Integer, Integer::divide, Integer, ref self, ref rhs);
+
+impl_single_binop!(Div, div, Integer, i8, Integer::divide_c_long, Integer, ref self);
+impl_single_binop!(Div, div, &Integer, i8, Integer::divide_c_long, Integer);
+impl_single_binop!(Div, div, Integer, &i8, Integer::divide_c_long, Integer, ref self, deref rhs);
+impl_single_binop!(Div, div, &Integer, &i8, Integer::divide_c_long, Integer, deref rhs);
+
+impl_single_binop!(Div, div, Integer, u8, Integer::divide_c_long, Integer, ref self);
+impl_single_binop!(Div, div, &Integer, u8, Integer::divide_c_long, Integer);
+impl_single_binop!(Div, div, Integer, &u8, Integer::divide_c_long, Integer, ref self, deref rhs);
+impl_single_binop!(Div, div, &Integer, &u8, Integer::divide_c_long, Integer, deref rhs);
+
+impl_single_binop!(Div, div, Integer, i16, Integer::divide_c_long, Integer, ref self);
+impl_single_binop!(Div, div, &Integer, i16, Integer::divide_c_long, Integer);
+impl_single_binop!(Div, div, Integer, &i16, Integer::divide_c_long, Integer, ref self, deref rhs);
+impl_single_binop!(Div, div, &Integer, &i16, Integer::divide_c_long, Integer, deref rhs);
+
+impl_single_binop!(Div, div, Integer, u16, Integer::divide_c_long, Integer, ref self);
+impl_single_binop!(Div, div, &Integer, u16, Integer::divide_c_long, Integer);
+impl_single_binop!(Div, div, Integer, &u16, Integer::divide_c_long, Integer, ref self, deref rhs);
+impl_single_binop!(Div, div, &Integer, &u16, Integer::divide_c_long, Integer, deref rhs);
+
+impl_single_binop!(Div, div, Integer, i32, Integer::divide_c_long, Integer, ref self);
+impl_single_binop!(Div, div, &Integer, i32, Integer::divide_c_long, Integer);
+impl_single_binop!(Div, div, Integer, &i32, Integer::divide_c_long, Integer, ref self, deref rhs);
+impl_single_binop!(Div, div, &Integer, &i32, Integer::divide_c_long, Integer, deref rhs);
+
+impl_single_binop!(Div, div, Integer, u32, Integer::divide_c_long, Integer, ref self);
+impl_single_binop!(Div, div, &Integer, u32, Integer::divide_c_long, Integer);
+impl_single_binop!(Div, div, Integer, &u32, Integer::divide_c_long, Integer, ref self, deref rhs);
+impl_single_binop!(Div, div, &Integer, &u32, Integer::divide_c_long, Integer, deref rhs);
+
+cfg_if::cfg_if! {
+    if #[cfg(all(target_pointer_width = "64", not(windows)))] {
+        impl_single_binop!(Div, div, Integer, i64, Integer::divide_c_long, Integer, ref self);
+        impl_single_binop!(Div, div, &Integer, i64, Integer::divide_c_long, Integer);
+        impl_single_binop!(Div, div, Integer, &i64, Integer::divide_c_long, Integer, ref self, deref rhs);
+        impl_single_binop!(Div, div, &Integer, &i64, Integer::divide_c_long, Integer, deref rhs);
+    } else {
+        impl_single_binop!(Div, div, Integer, i64, Integer::divide, Integer, ref self, into rhs);
+        impl_single_binop!(Div, div, &Integer, i64, Integer::divide, Integer, into rhs);
+        impl_single_binop!(Div, div, Integer, &i64, Integer::divide, Integer, ref self, into rhs);
+        impl_single_binop!(Div, div, &Integer, &i64, Integer::divide, Integer, into rhs);
+    }
+}
+
+impl_single_binop!(Div, div, Integer, u64, Integer::divide, Integer, ref self, into rhs);
+impl_single_binop!(Div, div, &Integer, u64, Integer::divide, Integer, into rhs);
+impl_single_binop!(Div, div, Integer, &u64, Integer::divide, Integer, ref self, into rhs);
+impl_single_binop!(Div, div, &Integer, &u64, Integer::divide, Integer, into rhs);
+
+impl_single_binop!(Div, div, Integer, i128, Integer::divide, Integer, ref self, into rhs);
+impl_single_binop!(Div, div, &Integer, i128, Integer::divide, Integer, into rhs);
+impl_single_binop!(Div, div, Integer, &i128, Integer::divide, Integer, ref self, into rhs);
+impl_single_binop!(Div, div, &Integer, &i128, Integer::divide, Integer, into rhs);
+
+impl_single_binop!(Div, div, Integer, u128, Integer::divide, Integer, ref self, into rhs);
+impl_single_binop!(Div, div, &Integer, u128, Integer::divide, Integer, into rhs);
+impl_single_binop!(Div, div, Integer, &u128, Integer::divide, Integer, ref self, into rhs);
+impl_single_binop!(Div, div, &Integer, &u128, Integer::divide, Integer, into rhs);
+
 // REMAINDER
 
 impl_single_binop!(
@@ -436,6 +510,22 @@ mod test {
 
         let string_repr = c.to_string();
         assert_eq!(&string_repr, "255050250");
+    }
+
+    #[test]
+    fn divide_integers() {
+        let a: Integer = "52384129912341238437480192384".parse().unwrap();
+
+        #[allow(clippy::eq_op)]
+        let one = &a / &a;
+        assert_eq!(one, 1);
+        assert_eq!(&a / 10_000_000_000_000_000_000_000u128, 5238412);
+
+        let b: Integer = 1_234_567.into();
+        assert_eq!(&b / 123_456, 10);
+        assert_eq!(&b / 3, 411_522);
+        assert_eq!(&b / -3, -411_522);
+        assert_eq!(&b / -230, -5_367)
     }
 
     #[test]
