bf0b7415f633d3dd93dec73a3f60b6840c401edc
Declan Kelly <dkelly.home@gmail.com>
2019-11-07 10:05:02 -0800

    Separate test, forma, and lint jobs (#3)
    * Separate test, forma, and lint jobs
    
    * Some workflow fixes
    
    * Use dunce to canonicalize paths
    
    * Install llvm on windows build
    
    * Fix ops/conversion/compare for u32 on windows
    
    * Remove duplicate OpAssigns
    
    * Add cfg_if for remainder impls on u32
    
    * Remove cfg_if from remainder.
    
    u32 remainders return Integers.
    
    * Fix test

--- src/integer/ops/multiplication.rs
+++ src/integer/ops/multiplication.rs
@@ -41,18 +41,23 @@ impl_single_binop!(Mul, mul, &Integer, i32, Integer::multiply_c_long, Integer);
 impl_single_binop!(Mul, mul, Integer, &i32, Integer::multiply_c_long_assign, Integer, ref self, deref rhs);
 impl_single_binop!(Mul, mul, &Integer, &i32, Integer::multiply_c_long, Integer, deref rhs);
 
-impl_single_binop!(Mul, mul, Integer, u32, Integer::multiply_c_long_assign, Integer, ref self);
-impl_single_binop!(Mul, mul, &Integer, u32, Integer::multiply_c_long, Integer);
-impl_single_binop!(Mul, mul, Integer, &u32, Integer::multiply_c_long_assign, Integer, ref self, deref rhs);
-impl_single_binop!(Mul, mul, &Integer, &u32, Integer::multiply_c_long, Integer, deref rhs);
-
 cfg_if::cfg_if! {
     if #[cfg(all(target_pointer_width = "64", not(windows)))] {
+        impl_single_binop!(Mul, mul, Integer, u32, Integer::multiply_c_long_assign, Integer, ref self);
+        impl_single_binop!(Mul, mul, &Integer, u32, Integer::multiply_c_long, Integer);
+        impl_single_binop!(Mul, mul, Integer, &u32, Integer::multiply_c_long_assign, Integer, ref self, deref rhs);
+        impl_single_binop!(Mul, mul, &Integer, &u32, Integer::multiply_c_long, Integer, deref rhs);
+
         impl_single_binop!(Mul, mul, Integer, i64, Integer::multiply_c_long_assign, Integer, ref self);
         impl_single_binop!(Mul, mul, &Integer, i64, Integer::multiply_c_long, Integer);
         impl_single_binop!(Mul, mul, Integer, &i64, Integer::multiply_c_long_assign, Integer, ref self, deref rhs);
         impl_single_binop!(Mul, mul, &Integer, &i64, Integer::multiply_c_long, Integer, deref rhs);
     } else {
+        impl_single_binop!(Mul, mul, Integer, u32, Integer::multiply_assign, Integer, ref self, into rhs);
+        impl_single_binop!(Mul, mul, &Integer, u32, Integer::multiply, Integer, into rhs);
+        impl_single_binop!(Mul, mul, Integer, &u32, Integer::multiply_assign, Integer, ref self, into rhs);
+        impl_single_binop!(Mul, mul, &Integer, &u32, Integer::multiply, Integer, into rhs);
+
         impl_single_binop!(Mul, mul, Integer, i64, Integer::multiply_assign, Integer, ref self, into rhs);
         impl_single_binop!(Mul, mul, &Integer, i64, Integer::multiply, Integer, into rhs);
         impl_single_binop!(Mul, mul, Integer, &i64, Integer::multiply_assign, Integer, ref self, into rhs);
@@ -138,20 +143,17 @@ impl_single_op_assign!(
 );
 impl_single_op_assign!(MulAssign, mul_assign, Integer, &i32, Integer::multiply_c_long_assign, deref rhs);
 
-impl_single_op_assign!(
-    MulAssign,
-    mul_assign,
-    Integer,
-    u32,
-    Integer::multiply_c_long_assign
-);
-impl_single_op_assign!(MulAssign, mul_assign, Integer, &u32, Integer::multiply_c_long_assign, deref rhs);
-
 cfg_if::cfg_if! {
     if #[cfg(all(target_pointer_width = "64", not(windows)))] {
+        impl_single_op_assign!(MulAssign, mul_assign, Integer, u32, Integer::multiply_c_long_assign);
+        impl_single_op_assign!(MulAssign, mul_assign, Integer, &u32, Integer::multiply_c_long_assign, deref rhs);
+
         impl_single_op_assign!(MulAssign, mul_assign, Integer, i64, Integer::multiply_c_long_assign);
         impl_single_op_assign!(MulAssign, mul_assign, Integer, &i64, Integer::multiply_c_long_assign, deref rhs);
     } else {
+        impl_single_op_assign!(MulAssign, mul_assign, Integer, u32, Integer::multiply_assign, into rhs);
+        impl_single_op_assign!(MulAssign, mul_assign, Integer, &u32, Integer::multiply_assign, into rhs);
+
         impl_single_op_assign!(MulAssign, mul_assign, Integer, i64, Integer::multiply_assign, into rhs);
         impl_single_op_assign!(MulAssign, mul_assign, Integer, &i64, Integer::multiply_assign, into rhs);
     }
