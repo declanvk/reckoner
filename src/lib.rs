5f5162b2963e482e0c4da6cf90e26d8f83e085a2
Declan Kelly <dkelly.home@gmail.com>
2019-11-23 20:07:36 -0800

    Clean gh-pages branch

--- src/lib.rs
+++
@@ -1,9 +0,0 @@
-#![deny(missing_docs)]
-
-//! Arbitrary precision integer and rational arithmetic library wrapping
-//! (imath)[https://github.com/creachadair/imath/]
-
-pub(crate) mod error;
-pub(crate) mod integer;
-
-pub use crate::{error::Error, integer::Integer};
