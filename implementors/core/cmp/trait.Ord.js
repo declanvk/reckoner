704424318aab79b869e9de55d4f527f3c9002f8e
declanvk <dkelly.home@gmail.com>
2020-01-05 04:26:38 +0000

    Deploying to gh-pages from master 38bea680e56b8f7399018b55ee4f56d943bbf56e

--- implementors/core/cmp/trait.Ord.js
+++ implementors/core/cmp/trait.Ord.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["reckoner"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"reckoner/struct.Integer.html\" title=\"struct reckoner::Integer\">Integer</a>",synthetic:false,types:["reckoner::integer::Integer"]},];
+implementors["reckoner"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"reckoner/struct.Integer.html\" title=\"struct reckoner::Integer\">Integer</a>",synthetic:false,types:["reckoner::integer::Integer"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"reckoner/struct.Rational.html\" title=\"struct reckoner::Rational\">Rational</a>",synthetic:false,types:["reckoner::rational::Rational"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
