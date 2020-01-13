cc520a6f4e648e9da5464e39cac59f6e081a2468
declanvk <dkelly.home@gmail.com>
2020-01-13 08:10:10 +0000

    Deploying to gh-pages from master 552c785f620e505f433439635ffad0e6afd75c73

---
+++ implementors/core/iter/traits/accum/trait.Product.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["reckoner"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/accum/trait.Product.html\" title=\"trait core::iter::traits::accum::Product\">Product</a>&lt;<a class=\"struct\" href=\"reckoner/struct.Integer.html\" title=\"struct reckoner::Integer\">Integer</a>&gt; for <a class=\"struct\" href=\"reckoner/struct.Integer.html\" title=\"struct reckoner::Integer\">Integer</a>",synthetic:false,types:["reckoner::integer::Integer"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/accum/trait.Product.html\" title=\"trait core::iter::traits::accum::Product\">Product</a>&lt;<a class=\"struct\" href=\"reckoner/struct.Rational.html\" title=\"struct reckoner::Rational\">Rational</a>&gt; for <a class=\"struct\" href=\"reckoner/struct.Rational.html\" title=\"struct reckoner::Rational\">Rational</a>",synthetic:false,types:["reckoner::rational::Rational"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
