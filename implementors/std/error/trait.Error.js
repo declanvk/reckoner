6e00ca2f339ee997873926cb4fd0ffca113f7f66
declanvk <dkelly.home@gmail.com>
2020-02-03 21:31:45 +0000

    Deploying to gh-pages from master - dcb73f5769350c2d00bbb1716b719a3e34bd6e86 🚀

--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["reckoner"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"reckoner/enum.Error.html\" title=\"enum reckoner::Error\">Error</a>",synthetic:false,types:["reckoner::error::Error"]},];
+implementors["reckoner"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"reckoner/enum.Error.html\" title=\"enum reckoner::Error\">Error</a>","synthetic":false,"types":["reckoner::error::Error"]}];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
