ae9b63b9edcd1e7113f5d2998205333540eca485
declanvk <dkelly.home@gmail.com>
2022-11-18 19:42:54 +0000

    Deploying to gh-pages from main @ 673ed4636897773daa9782a13b4c6a7db04697eb 🚀

--- implementors/core/convert/trait.Into.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["reckoner"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.64.0/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.64.0/std/primitive.u32.html\">u32</a>&gt; for <a class=\"enum\" href=\"reckoner/enum.RoundMode.html\" title=\"enum reckoner::RoundMode\">RoundMode</a>","synthetic":false,"types":["reckoner::rational::RoundMode"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
