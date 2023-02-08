1435c12d0eb81ca56ad980409ef29b289c452ca5
declanvk <dkelly.home@gmail.com>
2023-02-08 20:16:56 +0000

    Deploying to gh-pages from main @ 9390749e764235c68e0d2a888fe33d2b312b9ee2 🚀

--- implementors/core/marker/trait.StructuralPartialEq.js
+++ implementors/core/marker/trait.StructuralPartialEq.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"reckoner":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"reckoner/enum.Error.html\" title=\"enum reckoner::Error\">Error</a>"]]
+"reckoner":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.0/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"reckoner/enum.Error.html\" title=\"enum reckoner::Error\">Error</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
