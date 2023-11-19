470b2f7c2347d8eb6185eafe824ebe36aa67b71e
declanvk <dkelly.home@gmail.com>
2023-11-19 00:29:28 +0000

    Deploying to gh-pages from main @ f10e1ca702e5d914de08d32193e55b2820cef246 🚀

--- src-files.js
+++ src-files.js
@@ -1,5 +1,5 @@
-var sourcesIndex = JSON.parse('{\
+var srcIndex = JSON.parse('{\
 "creachadair_imath_sys":["",[],["lib.rs"]],\
 "reckoner":["",[["integer",[["ops",[],["addition.rs","addition_assign.rs","division.rs","division_assign.rs","multiplication.rs","multiplication_assign.rs","negation.rs","remainder.rs","remainder_assign.rs","subtraction.rs","subtraction_assign.rs"]]],["comparison.rs","conversions.rs","ops.rs"]],["rational",[["ops",[],["addition.rs","addition_assign.rs","division.rs","division_assign.rs","multiplication.rs","multiplication_assign.rs","negation.rs","subtraction.rs","subtraction_assign.rs"]]],["comparisons.rs","conversions.rs","ops.rs"]]],["error.rs","integer.rs","lib.rs","rational.rs"]]\
 }');
-createSourceSidebar();
+createSrcSidebar();
