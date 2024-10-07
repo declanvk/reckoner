abfaabdfb9c5cef9358da073dada557b77f25bcb
declanvk <dkelly.home@gmail.com>
2024-10-07 00:34:15 +0000

    Deploying to gh-pages from main @ 0296d100a3d72dc2366482293029dd0352221942 🚀

---
+++ trait.impl/core/iter/traits/accum/trait.Sum.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"reckoner":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.81.0/core/iter/traits/accum/trait.Sum.html\" title=\"trait core::iter::traits::accum::Sum\">Sum</a> for <a class=\"struct\" href=\"reckoner/struct.Integer.html\" title=\"struct reckoner::Integer\">Integer</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.81.0/core/iter/traits/accum/trait.Sum.html\" title=\"trait core::iter::traits::accum::Sum\">Sum</a> for <a class=\"struct\" href=\"reckoner/struct.Rational.html\" title=\"struct reckoner::Rational\">Rational</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ trait.impl/core/marker/trait.Copy.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"creachadair_imath_sys":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.81.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"creachadair_imath_sys/struct.mpq_t.html\" title=\"struct creachadair_imath_sys::mpq_t\">mpq_t</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.81.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"creachadair_imath_sys/struct.mpz_t.html\" title=\"struct creachadair_imath_sys::mpz_t\">mpz_t</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
